ee360738c94cc33baa872cfdcb8faded74268955
tomas-20 <tacuna20@stuy.edu>
2020-11-22 13:54:53 -0500

    added the basic functions for RationalNumber

--- RationalNumber.java
+++ RationalNumber.java
@@ -8,23 +8,25 @@ public class RationalNumber extends RealNumber {
   */
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
+    numerator = nume;
+    denominator = deno;
   }
 
   public double getValue(){
-    return 0.0;
+    return ((double) numerator) / denominator;
   }
 
   /**
   *@return the numerator
   */
   public int getNumerator(){
-    return 0;
+    return numerator;
   }
   /**
   *@return the denominator
   */
   public int getDenominator(){
-    return 0;
+    return denominator;
   }
   /**
   *@return a new RationalNumber that has the same numerator
