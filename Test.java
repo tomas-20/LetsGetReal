aae22cce612ea1ba71351d06011ed1e902bedca9
tomas-20 <tacuna20@stuy.edu>
2020-11-24 23:36:47 -0500

    just to remember what it was like at this time

--- Test.java
+++ Test.java
@@ -1,21 +1,23 @@
 public class Test {
+  public static void testStuff(RealNumber a, RealNumber b) {
+    String aS = a.toString();
+    String bS = b.toString();
+    System.out.println(aS);
+    System.out.println(bS);
+    System.out.println(aS + '=' + bS + '\n' + a.equals(b));
+    System.out.println(aS + '+' + bS + '=' + a.add(b));
+    System.out.println(aS + '-' + bS + '=' + a.subtract(b));
+    System.out.println(aS + '*' + bS + '=' + a.multiply(b));
+    System.out.println(aS + '/' + '(' + bS + ")=" + a.divide(b));
+    System.out.println(a.add(b));
+  }
   public static void main(String[] args) {
     RealNumber uno = new RealNumber(Double.parseDouble(args[0]));
     RealNumber dos = new RealNumber(Double.parseDouble(args[1]));
     RationalNumber tres = new RationalNumber(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
     RationalNumber cuatro = new RationalNumber(Integer.parseInt(args[4]), Integer.parseInt(args[5]));
-    System.out.println(uno.equals(dos));
-    System.out.println(uno.add(dos));
-    System.out.println(uno.multiply(dos));
-    System.out.println(uno.divide(dos));
-    System.out.println(uno.subtract(dos));
+    //doStuff(uno, dos);
     System.out.println();
-    System.out.println(tres);
-    System.out.println(cuatro);
-    System.out.println(tres.equals(cuatro));
-    System.out.println(tres.add(cuatro));
-    System.out.println(tres.multiply(cuatro));
-    System.out.println(tres.divide(cuatro));
-    System.out.println(tres.subtract(cuatro));
+    testStuff(tres, cuatro);
   }
 }
